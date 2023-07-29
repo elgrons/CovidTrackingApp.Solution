de67ec53019dece6ce1389e9e302cb572f54b03e
Eliot Gronstal <eliot.lauren@gmail.com>
2023-07-28 21:26:47 -0700

    update that state and date filters are functional.

--- Controllers/CovidCasesController.cs
+++ Controllers/CovidCasesController.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using CovidTracking.Models;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using System.Text.Json.Nodes;
-using System.Diagnostics;
 
 namespace CovidTracking.Controllers
 {
@@ -13,6 +10,34 @@ namespace CovidTracking.Controllers
         {
             var cases = CovidCase.GetCases().Result;
             return View(cases);
+        }
+        public async Task<IActionResult> FilterCases(string state, string date)
+        {
+            if (string.IsNullOrEmpty(date))
+            {
+                var response = await ApiHelper.GetCases();
+                var cases = JsonConvert.DeserializeObject<List<CovidCase>>(response);
+
+                if (!string.IsNullOrEmpty(state))
+                {
+                    cases = cases.Where(c => c.State.Equals(state, StringComparison.InvariantCultureIgnoreCase)).ToList();
+                }
+                return Json(cases);
+            }
+            else
+            {
+                var response = await ApiHelper.GetCasesByDate(date);
+                var singleCase = JsonConvert.DeserializeObject<CovidCase>(response);
+
+                if (!string.IsNullOrEmpty(state) && !singleCase.State.Equals(state, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    return Json(new List<CovidCase>());
+                }
+                else
+                {
+                    return Json(new List<CovidCase> { singleCase });
+                }
+            }
         }
     }
 }
